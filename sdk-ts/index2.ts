95efce73ba37533600666d4dc94b12cf384eea44
roll <eskarev@gmail.com>
2025-10-01 08:51:56 +0100

    Implemented typescript sdk

--- sdk-ts/index2.ts
+++
@@ -1,3 +0,0 @@
-import { z } from "zod"
-
-export default z.object({ "dealer": z.object({ "title": z.string(), "url": z.string().optional() }), "resources": z.tuple([z.object({ "name": z.literal("car"), "schema": z.literal("https://raw.githubusercontent.com/datisthq/cardealerdp/v0.1.1/schemas/car.json") })]) })
