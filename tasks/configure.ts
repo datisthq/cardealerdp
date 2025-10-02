56ed1a2d09d116ea8ed8681dff203c5e24bfa920
roll <eskarev@gmail.com>
2025-10-02 11:59:58 +0100

    Updated configure

--- tasks/configure.ts
+++ tasks/configure.ts
@@ -1,112 +1,66 @@
+import { rename } from "node:fs/promises"
+import { join } from "node:path"
 import { intro, isCancel, outro, spinner, text } from "@clack/prompts"
 import pc from "picocolors"
 import { remark } from "remark"
 import { replaceInFile } from "replace-in-file"
 import TOML from "smol-toml"
-import { readdir, rename } from "node:fs/promises"
-import { join } from "node:path"
+import metadata from "../package.json" with { type: "json" }
 
 const loader = spinner()
 intro(pc.bold("Welcome to the Data Package extension template!"))
 
-const name = await text({
-  message: "Provide a name for your extension",
+const inputSlug = await text({
+  message: "Provide a slug for your extension",
   placeholder: "extensiondp",
+  initialValue: metadata.slug,
   validate(value) {
-    if (value.length === 0) return "Name is required!"
+    if (value.length === 0) return "Title is required!"
     return undefined
   },
 })
 
-if (!isCancel(name)) {
-  loader.start("Setting the extension name...")
+const inputTitle = await text({
+  message: "Provide a title for your extension",
+  placeholder: "Extension DP",
+  initialValue: metadata.title,
+  validate(value) {
+    if (value.length === 0) return "Title is required!"
+    return undefined
+  },
+})
 
-  await replaceInFile({
-    files: ["sdk-ts/package.json"],
-    processor: source => {
-      const data = JSON.parse(source) as any
-      data.name = name
-      const target = JSON.stringify(data, null, 2)
-      return target
-    },
-  })
-
-  await replaceInFile({
-    files: ["sdk-py/pyproject.toml"],
-    processor: source => {
-      const data = TOML.parse(source) as any
-      data.project.name = name
-      const target = TOML.stringify(data)
-      return target
-    },
-  })
-
-  await replaceInFile({
-    files: [
-      "extension/README.md",
-      "sdk-py/README.md",
-      "sdk-ts/README.md",
-      "README.md",
-    ],
-    processor: source => {
-      const tree = remark().parse(source)
-
-      for (const node of tree.children) {
-        if (node.type === "heading" && node.depth === 1) {
-          node.children = [{ type: "text", value: name }]
-          break
-        }
-      }
-
-      return remark().stringify(tree)
-    },
-  })
-
-  // Rename the Python package directory
-  const sdkPyPath = "sdk-py"
-  const dirs = await readdir(sdkPyPath, { withFileTypes: true })
-  const packageDir = dirs.find(dir => dir.isDirectory())
-
-  if (packageDir) {
-    const oldPath = join(sdkPyPath, packageDir.name)
-    const newPath = join(sdkPyPath, name)
-    await rename(oldPath, newPath)
-  }
-
-  loader.stop("Name set!")
-}
-
-const description = await text({
+const inputDescription = await text({
   message: "Provide a description for your extension",
   placeholder: "A data package extension for...",
+  initialValue: metadata.description,
   validate(value) {
     if (value.length === 0) return "Description is required!"
     return undefined
   },
 })
 
-if (!isCancel(description)) {
-  loader.start("Setting the extension description...")
+const inputRepository = await text({
+  message: "Provide a repository URL for your extension",
+  placeholder: "https://github.com/datisthq/extensiondp",
+  initialValue: metadata.repository,
+  validate(value) {
+    if (value.length === 0) return "Repository is required!"
+    return undefined
+  },
+})
 
-  await replaceInFile({
-    files: ["sdk-ts/package.json"],
-    processor: source => {
-      const data = JSON.parse(source) as any
-      data.description = description
-      const target = JSON.stringify(data, null, 2)
-      return target
-    },
-  })
+const slug = isCancel(inputSlug) ? metadata.slug : inputSlug
+const title = isCancel(inputTitle) ? metadata.title : inputTitle
+const description = isCancel(inputDescription)
+  ? metadata.description
+  : inputDescription
+const repository = isCancel(inputRepository)
+  ? metadata.repository
+  : inputRepository
 
-  await replaceInFile({
-    files: ["sdk-py/pyproject.toml"],
-    processor: source => {
-      const data = TOML.parse(source) as any
-      data.project.description = description
-      const target = TOML.stringify(data)
-      return target
-    },
-  })
+if (title || description || repository) {
+  loader.start("Updating the extension...")
 
   await replaceInFile({
     files: [
@@ -119,13 +73,22 @@ if (!isCancel(description)) {
       const tree = remark().parse(source)
 
       for (const node of tree.children) {
-        if (node.type === "paragraph") {
-          const textNode = node.children[0]
-          if (textNode?.type === "text") {
-            const value = textNode.value.trim()
-            if (!value.startsWith("[") && !value.startsWith("<")) {
-              node.children = [{ type: "text", value: description }]
-              break
+        if (title) {
+          if (node.type === "heading" && node.depth === 1) {
+            node.children = [{ type: "text", value: title }]
+            break
+          }
+        }
+
+        if (description) {
+          if (node.type === "paragraph") {
+            const textNode = node.children[0]
+            if (textNode?.type === "text") {
+              const value = textNode.value.trim()
+              if (!value.startsWith("[") && !value.startsWith("<")) {
+                node.children = [{ type: "text", value: description }]
+                break
+              }
             }
           }
         }
@@ -135,7 +98,43 @@ if (!isCancel(description)) {
     },
   })
 
-  loader.stop("Description set!")
+  await replaceInFile({
+    files: ["package.json", "sdk-ts/package.json"],
+    processor: source => {
+      const data = JSON.parse(source) as any
+
+      if (slug) data.slug = slug
+      if (title) data.title = title
+      if (description) data.description = description
+      if (repository) data.repository = repository
+
+      const target = JSON.stringify(data, null, 2)
+      return target
+    },
+  })
+
+  await replaceInFile({
+    files: ["sdk-py/pyproject.toml"],
+    processor: source => {
+      const data = TOML.parse(source) as any
+
+      if (slug) data.project.slug = slug
+      if (title) data.project.title = title
+      if (description) data.project.description = description
+      if (repository) data.project.urls = { repository }
+
+      const target = TOML.stringify(data)
+      return target
+    },
+  })
+
+  // Rename the Python package directory
+  const sdkPath = "sdk-py"
+  const oldPath = join(sdkPath, metadata.slug)
+  const newPath = join(sdkPath, slug)
+  await rename(oldPath, newPath)
+
+  loader.stop("Extension is updated!")
 }
 
 outro("All done! Now you can review and commit the changes")
