341f72624c2f5412c8a7165df4069abef23c632e
roll <eskarev@gmail.com>
2025-10-02 10:49:48 +0100

    Updated configure

--- tasks/configure.ts
+++ tasks/configure.ts
@@ -1,13 +1,10 @@
-import { join } from "node:path"
 import { intro, isCancel, spinner, text } from "@clack/prompts"
 import pc from "picocolors"
+import { remark } from "remark"
 import { replaceInFile } from "replace-in-file"
+import TOML from "smol-toml"
 
 const loader = spinner()
-const root = join(import.meta.dirname, "..")
-
-console.log(root)
-
 intro(pc.bold("Welcome to the Data Package extension template!"))
 
 const name = await text({
@@ -19,27 +16,49 @@ const name = await text({
   },
 })
 
-if (isCancel(name)) {
-  process.exit(0)
+if (!isCancel(name)) {
+  loader.start("Setting the extension name...")
+
+  await replaceInFile({
+    files: ["sdk-ts/package.json"],
+    processor: source => {
+      const data = JSON.parse(source) as any
+      data.name = name
+      const target = JSON.stringify(data, null, 2)
+      return target
+    },
+  })
+
+  await replaceInFile({
+    files: ["sdk-py/pyproject.toml"],
+    processor: source => {
+      const data = TOML.parse(source) as any
+      data.project.name = name
+      const target = TOML.stringify(data)
+      return target
+    },
+  })
+
+  await replaceInFile({
+    files: [
+      "extension/README.md",
+      "sdk-py/README.md",
+      "sdk-ts/README.md",
+      "README.md",
+    ],
+    processor: source => {
+      const tree = remark().parse(source)
+
+      for (const node of tree.children) {
+        if (node.type === "heading" && node.depth === 1) {
+          node.children = [{ type: "text", value: name }]
+          break
+        }
+      }
+
+      return remark().stringify(tree)
+    },
+  })
+
+  loader.stop("Name set!")
 }
-
-loader.start(`Setting "${name}" as the extension name...`)
-
-await replaceInFile({
-  files: ["sdk-ts/package.json"],
-  from: /"name": ".*"/g,
-  to: `"name": "${name}"`,
-})
-
-await replaceInFile({
-  files: [
-    "extension/README.md",
-    "sdk-py/README.md",
-    "sdk-ts/README.md",
-    "README.md",
-  ],
-  from: /^# .*/g,
-  to: `# ${name}`,
-})
-
-loader.stop("Name set!")
