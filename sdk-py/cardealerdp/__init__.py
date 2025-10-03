06a0ad56ced48639ac46ac69782859f0b9fb330b
roll <eskarev@gmail.com>
2025-10-03 16:19:03 +0100

    Inherit from dplib in python

--- sdk-py/cardealerdp/__init__.py
+++ sdk-py/cardealerdp/__init__.py
@@ -1,5 +1,17 @@
-from .profile import Package
-from .profile import Resource
+from dplib import Package as StandardPackage
+from dplib import Resource as StandardResource
+from dplib import Dialect
+
+from .profile import Package as ExtensionPackage
+from .profile import Resource as ExtensionResource
 from .profile import Schema
 
 from .schemas import *
+
+class Resource(StandardResource, ExtensionResource):
+    pass
+
+class Package(StandardPackage, ExtensionPackage):
+  resources: list[Resource]
+
+__all__ = ["Package", "Resource", "Schema", "Dialect"]
--- sdk-ts/index.ts
+++ sdk-ts/index.ts
@@ -1,9 +1,9 @@
 import type * as standard from "@dpkit/core"
 import type * as extension from "./profile.ts"
 
-export type Package = standard.Package & extension.Package
-export type Resource = standard.Resource & extension.Resource
-export type Schema = extension.Schema
 export type Dialect = standard.Dialect
+export type Schema = extension.Schema
+export type Resource = standard.Resource & extension.Resource
+export type Package = standard.Package & extension.Package
 
 export type * from "./schemas/index.ts"
